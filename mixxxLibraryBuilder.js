136cb501fe64ddf0e1c6a7d06d6a879ab96b8f38
Nik Martin <nik.martin@gmail.com>
2018-08-24 17:26:40 -0500

    add MIT license lower default amount of files

--- mixxxLibraryBuilder.js
+++ mixxxLibraryBuilder.js
@@ -1,3 +1,13 @@
+/*
+* Copyright 2018 Nik Martin
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+  
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+  
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 /**
  * Mixxx Library Builder
  * using the smallest playable mp3 file (144 bytes), this script will generate a configurable amount of
@@ -5,7 +15,7 @@
  */
 
 // how many tracks to generate
-const trackCount = 1000;
+const trackCount = 10000;
 
 //how many artists per genre
 const artistCount = 8;
@@ -47,7 +57,7 @@ const buff = Buffer.alloc(mp3Base64String.length, mp3Base64String, 'base64');
 
 const tracksPerGenre = Math.floor(trackCount / genreList.length);
 genreList.forEach(genre => {
-  console.log('creating ', genre, ' genre');
+  console.log('creating', genre, 'genre');
   // make a dir for the genre
   try {
     fs.mkdirSync(`${libraryPath}/${genre}`);
